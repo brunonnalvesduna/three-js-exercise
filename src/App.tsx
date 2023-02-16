76833319c319ed5de1bc83af942b76d96fc9a175
Bruno Alves <bruno.alves@dunasystem.com>
2023-02-16 19:27:07 -0300

    point cloud

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import React, { ExoticComponent, ReactNode, useState } from 'react'
 import * as THREE from 'three'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { BufferGeometry, PointsMaterial } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 
 function App() {
   const sizes = { width: window.innerWidth/1.5, height: window.innerHeight/1.5}
   const scene = new THREE.Scene();
   const renderer = new THREE.WebGLRenderer();
-
-
   
   const triangle = () => {
     scene.clear();
     renderer.clear();
     
     const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 500);
-    // camera.clear();
     camera.position.set(0, 0, 5);
     camera.lookAt(0, 0, 0);
     
@@ -36,9 +36,10 @@ function App() {
     
     scene.add(line);
     renderer.setSize(sizes.width, sizes.height);
-    if(document.getElementById("renderContainer")?.firstElementChild!)
-    document.getElementById("renderContainer")?.removeChild(document.getElementById("renderContainer")?.firstElementChild!)
-    document.getElementById("renderContainer")?.appendChild(renderer.domElement)
+    if(document.getElementById("renderContainer")?.firstElementChild!){
+      document.getElementById("renderContainer")?.removeChild(document.getElementById("renderContainer")?.firstElementChild!);
+    };
+    document.getElementById("renderContainer")?.appendChild(renderer.domElement);
     renderer.render(scene, camera);
   }
 
@@ -53,19 +54,17 @@ function App() {
     scene.add(mesh);
 
     // Red Cube - Create Camera
-    const sizes = { width: 800, height: 600}
-    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 500 );
-    // camera.clear();
+    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 500);
     camera.position.set(0, 0, 5);
-
+    camera.lookAt(0, 0, 0);
 
     // Red Cube - Create Renderer
     renderer.setSize(sizes.width, sizes.height);
     if(document.getElementById("renderContainer")?.firstElementChild!){
-      console.log(document.getElementById("renderContainer")?.firstElementChild)
-      document.getElementById("renderContainer")?.removeChild(document.getElementById("renderContainer")?.firstElementChild!)
-  }
-    document.getElementById("renderContainer")?.appendChild(renderer.domElement)
+      console.log(document.getElementById("renderContainer")?.firstElementChild);
+      document.getElementById("renderContainer")?.removeChild(document.getElementById("renderContainer")?.firstElementChild!);
+  };
+    document.getElementById("renderContainer")?.appendChild(renderer.domElement);
 
     function animate() {
         requestAnimationFrame(animate);
@@ -76,12 +75,48 @@ function App() {
         animate();
   }
 
+  const pointCloud = () => {
+    scene.clear();
+    renderer.clear();
+
+    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 500);
+    camera.position.set(0, 0, 5);
+    camera.lookAt(0, 0, 0);
+
+    for (let i=0; i<100; i++) {
+      const pointColor = new THREE.PointsMaterial({color: `${'#' + Math.floor(Math.random()*16777215).toString(16)}`, size: 0.2})
+      const point = new THREE.Vector3((Math.random() - 0.5) * 2.5,(Math.random() - 0.5) * 2.5,(Math.random() - 0.5) * 2.5);
+      const geometry = new THREE.BufferGeometry().setFromPoints([point]);
+      const pointRendered = new THREE.Points(geometry, pointColor);
+      scene.add(pointRendered);
+    };
+
+    renderer.setSize(sizes.width, sizes.height);
+    if(document.getElementById("renderContainer")?.firstElementChild!){
+      console.log(document.getElementById("renderContainer")?.firstElementChild);
+      document.getElementById("renderContainer")?.removeChild(document.getElementById("renderContainer")?.firstElementChild!);
+    };
+    document.getElementById("renderContainer")?.appendChild(renderer.domElement);
+    
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    
+    const animate = () => {
+      controls.update();
+      renderer.render(scene, camera);
+      window.requestAnimationFrame(animate);
+    }
+
+    animate();
+  }
+
   return (
     <div className="App">
       <div>
         <div>
           <button onClick={cube}>Cube</button>
           <button onClick={triangle}>Triangle</button>
+          <button onClick={pointCloud}>Point Cloud</button>
         </div>
         <div id='renderContainer'>
         </div>
